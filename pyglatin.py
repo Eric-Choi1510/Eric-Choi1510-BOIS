ad8a0a47a923b5717326dc0de25be4bf76c6ff15
Eric-Choi1510 <64079979+Eric-Choi1510@users.noreply.github.com>
2020-04-26 15:51:13 +0900

    Add files via upload

---
+++ pyglatin.py
@@ -0,0 +1,11 @@
+pyg = 'ay'
+original = raw_input("Enter a word:" )
+word = original[1:]
+first = original[0]
+new_word = word + first + pyg
+if len(original) > 0 and original.isalpha():
+  print new_word
+else:
+  print "empty"
+
+  
